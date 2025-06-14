a498d44e6675f363fd6903d227861719cf6e8039
mariamalaa20 <159918273+mariamalaa20@users.noreply.github.com>
2025-06-14 17:03:29 +0300

    Update Alarm.java

--- Alarm.java
+++ Alarm.java
@@ -1,7 +1,6 @@
-public class Alarm implements IObserver{
-
+public class Alarm implements IObserver {
     @Override
-    public void guncelle(String mesaj) {
-        System.out.println("Alarm: "+mesaj+"\n");
+    public void update(String message) {
+        System.out.println("Alarm: " + message + "\n");
     }
 }
